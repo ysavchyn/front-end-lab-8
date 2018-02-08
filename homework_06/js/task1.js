64489fd14a32a1055be174b441886ff57dd06763
Yaroslav Savchyn <34660791+ysavchyn@users.noreply.github.com>
2018-02-08 21:54:53 +0200

    homework_06: add task1, add task2

---
+++ homework_06/js/task1.js
@@ -0,0 +1,33 @@
+let a = parseFloat(prompt("Enter side \"a\" of triangle:"));
+let b = parseFloat(prompt("Enter side \"b\" of triangle:"));
+let c = parseFloat(prompt("Enter side \"c\" of triangle:"));
+let type = "";
+
+if (a <= 0 || b <= 0 || c <= 0) {
+    console.log("Incorrect data");
+} else {
+    if (!(a + b > c && a + c > b && b + c > a)) {
+        console.log("Triangle does not exist");
+    } else {
+        let square = calcSquare(a, b, c);
+        if ((a * a + b * b == c * c) || (a * a + c * c == b * b) || (c * c + b * b == a * a)) {
+            type = " right ";
+        } else if (a == b && b == c && a == c) {
+            type = " equilateral ";
+        } else if (a == b || b == c || a == c) {
+            type += " isosceles ";
+        } else {
+            type = " scalene ";
+        }
+        if (parseInt(square) != square) {
+            console.log("Type of triangle is" + type + "triangle and square is " + square.toFixed(2));
+        } else {
+            console.log("Type of triangle is" + type + "triangle and square is " + Math.round(square));
+        }
+    }
+}
+
+function calcSquare(a, b, c) {
+    let p = (a + b + c) / 2;
+    return Math.sqrt(p * (p - a) * (p - b) * (p - c));
+}
