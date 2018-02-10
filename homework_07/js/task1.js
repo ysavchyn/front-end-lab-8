0a912c59fafbd414b956039658566f5949858093
ysavchyn <ysavchyn@outlook.com>
2018-02-10 19:20:15 +0200

    Final commit (1 task)
    Optimize code
    Build pyramide first and console.log instead console.log every row of pyramide

--- homework_07/js/task1.js
+++ homework_07/js/task1.js
@@ -1,20 +1,17 @@
-let number = Number(prompt("Enter natural number from 1 to 20:"));
-let pyramideBlock = "[~]";
-let pyramideResult = "";
-let blockCountInRow = -1;
-let indent = 0;
-
+let number = Number(prompt("Enter natural number from 1 to 20:")),
+    pyramideBlock = "[~]",
+    pyramideResult = "",
+    blockCountInRow = -1,
+    indent = 0;
 if (number <= 0 || number > 20 || !Number.isInteger(number)) {
     console.log("Incorrect number! Reload page");
 } else {
     for (let i = 1; i <= number; i++) {
-        for (let j = 1; j <= (number - 1 - indent); j++) {
+        for (let j = 1; j < number - indent; j++) {
             pyramideResult += "   ";
         }
-        blockCountInRow += 2;
-        pyramideResult += pyramideBlock.repeat(blockCountInRow);
+        pyramideResult += pyramideBlock.repeat(blockCountInRow += 2) + "\n";
         indent++;
-        console.log(pyramideResult + "\n");
-        pyramideResult = "";
     }
+    console.log(pyramideResult);
 }
