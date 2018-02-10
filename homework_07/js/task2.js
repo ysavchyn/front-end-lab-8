595a39e8f7e91f7272e6541f4b2fd1047a108a77
ysavchyn <ysavchyn@outlook.com>
2018-02-10 20:43:54 +0200

    Final commit
    Optimize code
    Delete all own functions according to answers in Gitter (Oleksandr Poliarush)
    Add checking for empty input data
    Add checking for white spaces input data

--- homework_07/js/task2.js
+++ homework_07/js/task2.js
@@ -1,74 +1,85 @@
-let startGame = confirm("Do you want to play a game?");
-let continueGame = true;
-let randomNumber = 0,
+let startGame = confirm("Do you want to play a game?"),
+    continueGame = true,
     minOfInterval = 0,
     maxOfInterval = 5,
-    inputNumber = null,
+    inputNumber,
     totalPrize = 0,
     currentAttemptPrize = 0,
-    maxStartAttemptPrize = 10;
+    maxAttemptPrize = 10;
 
-playGame();
-
-function playGame() {
+if (startGame) {
     while (continueGame) {
-        currentAttemptPrize = parseInt(maxStartAttemptPrize);
-        if (startGame) {
-            randomNumber = getRandomNumber(minOfInterval, maxOfInterval);
-            
-            //console.log(randomNumber); // For debug
-            
-            for (let attempts = 3; attempts > 0; attempts--) {
-                if (attempts < 3) {
-                    currentAttemptPrize = parseInt(currentAttemptPrize / 2);
-                }
-                inputNumber = prompt("Enter a number from " + minOfInterval + " to " + maxOfInterval + ":" + "\nAttempts left: " + attempts + "\nTotal prize: " + totalPrize + "$" + "\nPossible prize on current attempt: " + currentAttemptPrize + "$");
-                if (inputNumber == null) {
-                    return;
-                }
-                if (inputNumber == randomNumber) {
-                    if (attempts == 3) {
-                        totalPrize += currentAttemptPrize;
-                    }
-                    if (attempts == 2) {
-                        totalPrize += currentAttemptPrize;
-                    }
-                    if (attempts == 1) {
-                        totalPrize += currentAttemptPrize;
-                    }
-                    continueGame = confirm("Continue a game?");
-                    if (continueGame) {
-                        maxOfInterval *= 2;
-                        maxStartAttemptPrize *= 3;
-                        break;
-                    } else {
-                        continueGameFunc();
-                        break;
-                    }
-                } else if (attempts == 1 && inputNumber != randomNumber) {
-                    continueGameFunc();
+        currentAttemptPrize = parseInt(maxAttemptPrize);
+        let randomNumber = Math.floor(minOfInterval + Math.random() * (maxOfInterval + 1 - minOfInterval));
+        
+        //console.log(randomNumber); // For debug
+        
+        for (let attempts = 3; attempts > 0; attempts--) {
+            if (attempts < 3) {
+                currentAttemptPrize = parseInt(currentAttemptPrize / 2);
+            }
+            inputNumber = prompt("Enter a number from " + minOfInterval + " to " + maxOfInterval + ":" + "\nAttempts left: " + attempts + "\nTotal prize: " + totalPrize + "$" + "\nPossible prize on current attempt: " + currentAttemptPrize + "$");
+            if (inputNumber == null) {
+                continueGame = confirm("Thank you for a game. Your prize is: " + totalPrize + "$\nPlay again ?");
+                if (continueGame) {
+                    totalPrize = 0;
+                    maxOfInterval = 5;
+                    maxAttemptPrize = 10;
+                    break;
+                } else {
+                    continueGame = false;
                     break;
                 }
             }
-        } else {
-            console.log("You did not become a millionaire!");
-            return;
+            if ((/^\s+$/.test(inputNumber) || inputNumber.trim() == "") && attempts > 1) {
+                continue;
+            } else if ((/^\s+$/.test(inputNumber) || inputNumber.trim() == "") && attempts == 1) {
+                continueGame = confirm("Thank you for a game. Your prize is: " + totalPrize + "$\nPlay again ?");
+                if (continueGame) {
+                    totalPrize = 0;
+                    maxOfInterval = 5;
+                    maxAttemptPrize = 10;
+                    break;
+                } else {
+                    continueGame = false;
+                    break;
+                }
+            }
+            if (inputNumber == randomNumber) {
+                if (attempts >= 1) {
+                    totalPrize += currentAttemptPrize;
+                }
+                continueGame = confirm("Continue a game?");
+                if (continueGame) {
+                    maxOfInterval *= 2;
+                    maxAttemptPrize *= 3;
+                    break;
+                } else {
+                    continueGame = confirm("Thank you for a game. Your prize is: " + totalPrize + "$\nPlay again ?");
+                    if (continueGame) {
+                        totalPrize = 0;
+                        maxOfInterval = 5;
+                        maxAttemptPrize = 10;
+                        break;
+                    } else {
+                        continueGame = false;
+                        break;
+                    }
+                }
+            } else if (attempts == 1 && inputNumber != randomNumber) {
+                continueGame = confirm("Thank you for a game. Your prize is: " + totalPrize + "$\nPlay again ?");
+                if (continueGame) {
+                    totalPrize = 0;
+                    maxOfInterval = 5;
+                    maxAttemptPrize = 10;
+                    break;
+                } else {
+                    continueGame = false;
+                    break;
+                }
+            }
         }
     }
-}
-
-function getRandomNumber(minOfInterval, maxOfInterval) {
-    let randomNumber = minOfInterval + Math.random() * (maxOfInterval + 1 - minOfInterval);
-    return Math.floor(randomNumber);
-}
-
-function continueGameFunc() {
-    continueGame = confirm("Thank you for a game. Your prize is: " + totalPrize + "$\nPlay again ?");
-    if (continueGame) {
-        totalPrize = 0;
-        maxOfInterval = 5;
-        maxStartAttemptPrize = 10;
-    } else {
-        return;
-    }
+} else {
+    console.log("You did not become a millionaire!");
 }
