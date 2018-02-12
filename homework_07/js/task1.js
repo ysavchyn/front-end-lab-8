d0e6042344f33782cc87de1a55718b48c0a0e30d
ysavchyn <ysavchyn@outlook.com>
2018-02-13 01:28:51 +0200

    let in 2 rows

--- homework_07/js/task1.js
+++ homework_07/js/task1.js
@@ -1,8 +1,5 @@
-let number = Number(prompt("Enter natural number from 1 to 20:")),
-    pyramideBlock = "[~]",
-    pyramideResult = "",
-    blockCountInRow = -1,
-    indent = 0;
+let number = Number(prompt("Enter natural number from 1 to 20:")), pyramideBlock = "[~]",
+pyramideResult = "", blockCountInRow = -1, indent = 0;
 if (number <= 0 || number > 20 || !Number.isInteger(number)) {
     console.log("Incorrect number! Reload page");
 } else {
