73b5a815a114b94a56e7f3454a8d159a1d76b951
ysavchyn <ysavchyn@outlook.com>
2018-02-13 00:50:48 +0200

    Fix
    -Optimized code
    -Cancel button -> -1 attempt, not break while, when attemps > 1
    -fix excess ifs

--- homework_07/js/task2.js
+++ homework_07/js/task2.js
@@ -11,62 +11,19 @@ if (startGame) {
     while (continueGame) {
         currentAttemptPrize = parseInt(maxAttemptPrize);
         let randomNumber = Math.floor(minOfInterval + Math.random() * (maxOfInterval + 1 - minOfInterval));
-        
-        //console.log(randomNumber); // For debug
-        
+        //console.log(randomNumber); // For debug 
         for (let attempts = 3; attempts > 0; attempts--) {
-            if (attempts < 3) {
-                currentAttemptPrize = parseInt(currentAttemptPrize / 2);
-            }
             inputNumber = prompt("Enter a number from " + minOfInterval + " to " + maxOfInterval + ":" + "\nAttempts left: " + attempts + "\nTotal prize: " + totalPrize + "$" + "\nPossible prize on current attempt: " + currentAttemptPrize + "$");
-            if (inputNumber == null) {
-                continueGame = confirm("Thank you for a game. Your prize is: " + totalPrize + "$\nPlay again ?");
-                if (continueGame) {
-                    totalPrize = 0;
-                    maxOfInterval = 5;
-                    maxAttemptPrize = 10;
-                    break;
-                } else {
-                    continueGame = false;
-                    break;
-                }
-            }
-            if ((/^\s+$/.test(inputNumber) || inputNumber.trim() == "") && attempts > 1) {
-                continue;
-            } else if ((/^\s+$/.test(inputNumber) || inputNumber.trim() == "") && attempts == 1) {
-                continueGame = confirm("Thank you for a game. Your prize is: " + totalPrize + "$\nPlay again ?");
-                if (continueGame) {
-                    totalPrize = 0;
-                    maxOfInterval = 5;
-                    maxAttemptPrize = 10;
-                    break;
-                } else {
-                    continueGame = false;
-                    break;
-                }
-            }
-            if (inputNumber == randomNumber) {
-                if (attempts >= 1) {
-                    totalPrize += currentAttemptPrize;
-                }
+            if (inputNumber == randomNumber && (inputNumber.trim() != "")) {
+                totalPrize += currentAttemptPrize;
                 continueGame = confirm("Continue a game?");
                 if (continueGame) {
                     maxOfInterval *= 2;
                     maxAttemptPrize *= 3;
                     break;
-                } else {
-                    continueGame = confirm("Thank you for a game. Your prize is: " + totalPrize + "$\nPlay again ?");
-                    if (continueGame) {
-                        totalPrize = 0;
-                        maxOfInterval = 5;
-                        maxAttemptPrize = 10;
-                        break;
-                    } else {
-                        continueGame = false;
-                        break;
-                    }
                 }
-            } else if (attempts == 1 && inputNumber != randomNumber) {
+            }
+            if (!continueGame || attempts == 1) {
                 continueGame = confirm("Thank you for a game. Your prize is: " + totalPrize + "$\nPlay again ?");
                 if (continueGame) {
                     totalPrize = 0;
@@ -77,6 +34,9 @@ if (startGame) {
                     continueGame = false;
                     break;
                 }
+            } else {
+                currentAttemptPrize = parseInt(currentAttemptPrize / 2);
+                continue;
             }
         }
     }
