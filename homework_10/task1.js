78b2cced6c2de22902cd7a5431cb9525a3bd4439
ysavchyn <ysavchyn@outlook.com>
2018-02-21 22:31:03 +0200

    task1: commented example code

--- homework_10/task1.js
+++ homework_10/task1.js
@@ -12,26 +12,26 @@ function debounce(callBack, msDelay) {
 }
 
 // Example
-
-let iterator = 0;
-
-function increaseIteratorBy1() {
-  iterator++;
-  printIteratorValue();
-}
-
-function printIteratorValue() {
-  console.log('Iterator value ', iterator);
-}
-
-let increaseIterator = debounce(increaseIteratorBy1, 1000);
-
-increaseIterator();
-increaseIterator();
-increaseIterator();
-increaseIterator();
-increaseIterator();
-increaseIterator();
-increaseIterator();
-increaseIterator();
-increaseIterator(); // Should print 'Iterator value 2'
+//
+// let iterator = 0;
+//
+// function increaseIteratorBy1() {
+//   iterator++;
+//   printIteratorValue();
+// }
+//
+// function printIteratorValue() {
+//   console.log('Iterator value ', iterator);
+// }
+//
+// let increaseIterator = debounce(increaseIteratorBy1, 1000);
+//
+// increaseIterator();
+// increaseIterator();
+// increaseIterator();
+// increaseIterator();
+// increaseIterator();
+// increaseIterator();
+// increaseIterator();
+// increaseIterator();
+// increaseIterator(); // Should print 'Iterator value 1'
