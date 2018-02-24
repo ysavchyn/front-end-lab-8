783d6f57a6d45c4103f3b52c1b7decf95f9d40c7
Yaroslav Savchyn <34660791+ysavchyn@users.noreply.github.com>
2018-02-24 16:41:07 +0200

    homework_11: beta
    need to optimization of code

---
+++ homework_11/app.js
@@ -0,0 +1,52 @@
+let rootNode = document.getElementById("root"), id = 0,
+    getTree = (treeArray, node, display = "block") => {
+        let ul = document.createElement("ul");
+        ul.style.display = display;
+        node.appendChild(ul);
+        for (let item in treeArray) {
+            let li = document.createElement("li"), a = document.createElement("a"),
+                icon = document.createElement("i"), opened = false;
+            li.setAttribute("id", id);
+            ul.appendChild(li);
+            a.setAttribute("href", "#");
+            if (treeArray[item].hasOwnProperty("folder")) {
+                icon.setAttribute("class", "material-icons");
+                icon.innerHTML = "folder";
+                a.onclick = function(e) {
+                    let targetElement = document.getElementById(Number(e.target.parentElement.id));
+                    if (opened === true) {
+                        icon.innerHTML = "folder";
+                        targetElement.lastChild.style.display = "none";
+                        opened = false;
+                    } else {
+                        icon.innerHTML = "folder_open";
+                        targetElement.lastChild.style.display = "block";
+                        opened = true;
+                    }
+                }
+            } else {
+                icon.setAttribute("class", "material-icons file-icon");
+                icon.innerHTML = "insert_drive_file";
+            }
+            li.appendChild(icon);
+            li.appendChild(a);
+            a.innerHTML = treeArray[item].title;
+            let newNode = document.getElementById(id);
+            id++;
+            if ((treeArray[item].children === false || treeArray[item].children === null) && treeArray[item].hasOwnProperty("children")) {
+                let newUl = document.createElement("ul"), newLi = document.createElement("li");
+                newUl.style.display = display;
+                newLi.id = id;
+                newNode.appendChild(newUl);
+                newUl.appendChild(newLi);
+                em = document.createElement("em");
+                newLi.appendChild(em);
+                em.innerHTML = "Folder is empty";
+                id++;
+                continue;
+            } else {
+                getTree(treeArray[item].children, newNode, "none");
+            }
+        }
+    }
+getTree(structure, rootNode);
